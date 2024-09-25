68c84f6f1ca5b2953bccb663268a6a367202c03f
george-sw <gsw.l2c@gmail.com>
2024-09-25 21:46:27 +0100

    update, added player instance and drew on screen

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import sys
 import pygame
 from constants import *
+from player import *
 
 def main():
-  print(sys.prefix != sys.base_prefix, sys.prefix, sys.base_prefix)
-  print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+  # print(sys.prefix != sys.base_prefix, sys.prefix, sys.base_prefix)
+  # print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
   pygame.init()
   is_pygame_init = pygame.get_init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   running = True
   clock = pygame.time.Clock()
   delta_time = 0
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
   while running:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
     screen.fill("black")
+    player.draw(screen)
     pygame.display.flip()
     delta_time = clock.tick(60) / 1000
 
