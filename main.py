e2e1eca51ce2e3ceb7e74ba8d0f56ad28d91c860
george-sw <gsw.l2c@gmail.com>
2024-10-02 13:55:53 +0100

    added groups to game to make managing game objects easier

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-import pygame
+import pygame as pg
 from constants import *
 from player import *
 
 def main():
   # print(sys.prefix != sys.base_prefix, sys.prefix, sys.base_prefix)
   # print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-  pygame.init()
-  is_pygame_init = pygame.get_init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  pg.init()
+  is_pygame_init = pg.get_init()
+  screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   running = True
-  clock = pygame.time.Clock()
+  clock = pg.time.Clock()
   delta_time = 0
+  updatable = pg.sprite.Group()
+  drawable = pg.sprite.Group()
+  Player.containers = updatable, drawable
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
   while running:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
+    for event in pg.event.get():
+      if event.type == pg.QUIT:
         return
+    for game_object in updatable:
+      game_object.update(delta_time)
     screen.fill("black")
-    player.update(delta_time)
-    player.draw(screen)
-    pygame.display.flip()
+    for game_object in drawable:
+      game_object.draw(screen)
+    pg.display.flip()
     delta_time = clock.tick(60) / 1000
-    # print(delta_time)
 
 if __name__ == "__main__": 
   main()
