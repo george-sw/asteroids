a6eba6f07ed2bfc65713729dac402b87aed564fc
george-sw <gsw.l2c@gmail.com>
2024-10-03 11:53:49 +0100

    added rate limit to player shot

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.shot_timer = 0
 
   # in the player class
   def triangle(self):
@@ -24,6 +25,7 @@ class Player(CircleShape):
     self.rotation += PLAYER_TURN_SPEED * dt
 
   def update(self, dt):
+    self.shot_timer -= dt
     keys = pg.key.get_pressed()
 
     if keys[pg.K_a]:
@@ -35,12 +37,13 @@ class Player(CircleShape):
     if keys[pg.K_s]:
       self.move(-dt)
     if keys[pg.K_SPACE]:
-      self.shoot(dt)
+      if self.shot_timer < 0: self.shoot(dt) 
   def move(self, dt):
     forward = pg.Vector2(0, 1).rotate(self.rotation)
     self.position += forward * PLAYER_SPEED * dt
 
   def shoot(self, dt):
+    self.shot_timer = PLAYER_SHOT_RATE_LIMIT 
     shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
     # shot.velocity = pg.Vector2(0, 1).rotate(self.rotation)
     # shot.position += shot.velocity * PLAYER_SHOT_SPEED * dt
